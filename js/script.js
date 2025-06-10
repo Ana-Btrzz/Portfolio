67405cdbea61713c9d7f448dc0336c85a8d42329
Ana-Btrzz <anabeatriz.arruda7@gmail.com>
2025-06-10 15:00:05 -0300

    slide projetos

--- js/script.js
+++ js/script.js
@@ -48,3 +48,34 @@ function activateDescription() {
 }
 
 activateDescription();
+
+new Swiper(".card-wrapper", {
+  loop: true,
+  spaceBetween: 60,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  //responsive breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
